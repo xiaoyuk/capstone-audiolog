f3efa74cce0c52b17e592bcaf534c32f942a701d
Xiaoyu Ke <lilysoul14@gmail.com>
2018-07-29 08:52:19 +0800

    add springboot

---
+++ lc101/cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,61 @@
+package org.launchcode.controllers;
+
+import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+
+
+   @RequestMapping(value = "")
+    public String index(Model model) {
+
+
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "My Cheeses");
+
+
+        return "cheese/index";
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "Cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+       CheeseData.add(newCheese);
+        //Redirect to /cheese
+        return "redirect:";
+    }
+
+    @RequestMapping(value="remove", method=RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+       model.addAttribute("cheeses", CheeseData.getAll());
+       model.addAttribute("title", "Remove Cheese");
+       return "cheese/remove";
+    }
+
+    @RequestMapping(value="remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+       for (int cheeseId : cheeseIds) {
+           CheeseData.remove(cheeseId);
+       }
+       return "redirect:";
+    }
+
+
+
+}
---
+++ lc101/cheese-mvc/src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -0,0 +1,45 @@
+package org.launchcode.cheesemvc.models;
+
+public class Cheese {
+    private String name;
+    private String description;
+    private int cheeseId;
+    private static int nextId = 1;
+
+    public Cheese(String name, String description) {
+        //call the default constructor
+        this();
+        this.name = name;
+        this.description = description;
+    }
+
+    //default constructor
+    public Cheese() {
+        cheeseId = nextId;
+        nextId++;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
