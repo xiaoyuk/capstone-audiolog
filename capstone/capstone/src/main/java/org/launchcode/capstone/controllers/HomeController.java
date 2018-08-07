13182cceff6b692f64925c1436a86ebedf72622c
Xiaoyu Ke <lilysoul14@gmail.com>
2018-08-07 21:50:05 +0800

    initial commit

--- capstone/capstone/src/main/java/org/launchcode/capstone/controllers/HomeController.java
+++ capstone/capstone/src/main/java/org/launchcode/capstone/controllers/HomeController.java
@@ -1,1 +1,17 @@
-package org.launchcode.capstone.controllers;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.ResponseBody;@Controllerpublic class HomeController {    @RequestMapping(value = "")    @ResponseBody    public String index() {        return "Welcome to audio log!";    }}
+package org.launchcode.capstone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "Welcome to audio log!";
+    }
+
+
+}
